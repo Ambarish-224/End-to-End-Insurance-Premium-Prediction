34be8d7187c4cca330230bff5331e0c3a21f9840
Ambarish_224 <ambarish.singh224@gmal.com>
2023-02-24 10:53:08 +0530

    Setup file Completed

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+requirement_file_name = "requirements.txt"
+REMOVE_PACKAGE = "-e ."
+
+def get_requirements() -> List[str]:
+    with open(requirement_file_name) as requirement_file:
+        requirement_list = requirement_file.readline()
+    requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
+
+    if REMOVE_PACKAGE in requirement_list:
+        requirement_list.remove(REMOVE_PACKAGE)
+    return requirement_list
+
+
+setup(name='Insurance',
+      version='0.0.1',
+      description='Insurance Industry Level Project',
+      author='Ambarish Singh',
+      author_email='ambarish.gaya224@gmail.com',
+      packages=find_packages(),
+      install_requires=get_requirements()
+     )
